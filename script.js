67423f246a1be8faee3e05259976d13adbeba9b1
unknown <macadrian20@gmail.com>
2024-03-10 14:41:13 -0700

    created CE,C,DEL, / function

--- script.js
+++ script.js
@@ -2,44 +2,129 @@ const currentEntry = document.getElementById("current-entry");
 let enteredText = ''; // append entry to entered text
 currentEntry.innerText = enteredText;
 // init var for inner text 
+const savedNumber = document.getElementById("saved-number");
+let previousNumber = '';
+savedNumber.innerText = previousNumber;
+
+let operator = ''
+
+
+// Entered text needs to get moved to previous text
+// operation needs to separate previous and current input
+//
 
 
 
-
-// geteleemnetby id each number button 
+//BUTTONS
 const one = document.getElementById("one");
 one.addEventListener("click",() =>{
-    enteredText += 4;
+    enteredText += 1;
     currentEntry.innerText = enteredText;
 });
 
 
 const two = document.getElementById("two");
-two.style.color = "red";
+two.addEventListener("click",() =>{
+    enteredText += 2;
+    currentEntry.innerText = enteredText;
+});
+
 
 const three = document.getElementById("three");
-three.style.color = "red";
+three.addEventListener("click",() =>{
+    enteredText += 3;
+    currentEntry.innerText = enteredText;
+});
+
 
 const four = document.getElementById("four");
-four.style.color = "red";
+four.addEventListener("click",() =>{
+    enteredText += 4;
+    currentEntry.innerText = enteredText;
+});
+
 
 const five = document.getElementById("five");
-five.style.color = "red";
+five.addEventListener("click",() =>{
+    enteredText += 5;
+    currentEntry.innerText = enteredText;
+});
+
 
 const six = document.getElementById("six");
-six.style.color = "red";
+six.addEventListener("click",() =>{
+    enteredText += 6;
+    currentEntry.innerText = enteredText;
+});
+
 
 const seven = document.getElementById("seven");
-seven.style.color = "red";
+seven.addEventListener("click",() =>{
+    enteredText += 7;
+    currentEntry.innerText = enteredText;
+});
+
 
 const eight = document.getElementById("eight");
-eight.style.color = "red";
+eight.addEventListener("click",() =>{
+    enteredText += 8;
+    currentEntry.innerText = enteredText;
+});
+
 
 const nine = document.getElementById("nine");
-nine.style.color = "red";
+nine.addEventListener("click",() =>{
+    enteredText += 9;
+    currentEntry.innerText = enteredText;
+});
+
 
 const zero = document.getElementById("zero");
-zero.style.color = "red";
+zero.addEventListener("click",() =>{
+    enteredText += 0;
+    currentEntry.innerText = enteredText;
+});
+
+//----------------------------------------------------------------------------------
+
+
+//FUNCTION BUTTONS
+const divide = document.getElementById("divide");
+divide.addEventListener("click",() =>{
+    if(enteredText != ""){
+        previousNumber += enteredText;
+        enteredText = ''
+        currentEntry.innerText = enteredText;
+        savedNumber.innerText = previousNumber;
+    }
+});
+
+
+const clearEntry = document.getElementById("clear-entry");
+clearEntry.addEventListener("click",() =>{
+    enteredText = '';
+    currentEntry.innerText = enteredText;
+});
+
+const clear = document.getElementById("clear");
+clear.addEventListener("click",() =>{
+    enteredText = '';
+    previousNumber ='';
+    operator = '';
+    currentEntry.innerText = enteredText;
+    savedNumber.innerText = previousNumber;
+});
+
+const del = document.getElementById("delete");
+del.addEventListener("click", ()=>{
+    enteredText = enteredText.slice(0,-1);
+    currentEntry.innerText = enteredText;
+});
+
+// on press: 1.) push information in currently entered into previous, and clear currently entered. 
+//2.) be ready to take in new currentlyEntered
+//3.) apply appropriate function to both variables 
+
 
 
 // numButton append to output
