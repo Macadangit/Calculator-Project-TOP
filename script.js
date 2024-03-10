34a2ee75629e67b10d7240a6cdcf400684fc6a88
unknown <macadrian20@gmail.com>
2024-03-10 13:07:25 -0700

    Created first working button model

---
+++ script.js
@@ -0,0 +1,48 @@
+const currentEntry = document.getElementById("current-entry");
+let enteredText = ''; // append entry to entered text
+currentEntry.innerText = enteredText;
+// init var for inner text 
+
+
+
+
+// geteleemnetby id each number button 
+const one = document.getElementById("one");
+one.addEventListener("click",() =>{
+    enteredText += 4;
+    currentEntry.innerText = enteredText;
+});
+
+
+const two = document.getElementById("two");
+two.style.color = "red";
+
+const three = document.getElementById("three");
+three.style.color = "red";
+
+const four = document.getElementById("four");
+four.style.color = "red";
+
+const five = document.getElementById("five");
+five.style.color = "red";
+
+const six = document.getElementById("six");
+six.style.color = "red";
+
+const seven = document.getElementById("seven");
+seven.style.color = "red";
+
+const eight = document.getElementById("eight");
+eight.style.color = "red";
+
+const nine = document.getElementById("nine");
+nine.style.color = "red";
+
+const zero = document.getElementById("zero");
+zero.style.color = "red";
+
+
+// numButton append to output
+// function buttons do the functions
+// Saved functionality
+
