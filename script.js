16159dcaefc7c7622703a574d1a83cec424c2ec6
unknown <macadrian20@gmail.com>
2024-03-10 23:44:26 -0700

    Created all functions for all buttons. All functional, but not bulletproof.

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ let operator = ''
 
 
 
-//BUTTONS
+//Number buttons
 const one = document.getElementById("one");
 one.addEventListener("click",() =>{
     enteredText += 1;
@@ -85,27 +85,41 @@ zero.addEventListener("click",() =>{
     currentEntry.innerText = enteredText;
 });
 
+const point = document.getElementById("point");
+point.addEventListener("click", ()=> {
+    if(!enteredText.includes(".")){
+        enteredText += ".";
+        currentEntry.innerText = enteredText;
+    };
+});
+
 //----------------------------------------------------------------------------------
 
 
 //FUNCTION BUTTONS
-const divide = document.getElementById("divide");
-divide.addEventListener("click",() =>{
-    if(enteredText != ""){
-        previousNumber += enteredText;
-        enteredText = ''
-        currentEntry.innerText = enteredText;
-        savedNumber.innerText = previousNumber;
+
+
+//ROW ONE
+
+//% Function
+const percent = document.getElementById("percent");
+
+percent.addEventListener("click", () => {
+    if (currentEntry != '') {
+        enteredText = enteredText / 100;
+        currentEntry.innerText = enteredText; 
     }
 });
 
 
+//CE function
 const clearEntry = document.getElementById("clear-entry");
 clearEntry.addEventListener("click",() =>{
     enteredText = '';
     currentEntry.innerText = enteredText;
 });
 
+//C function
 const clear = document.getElementById("clear");
 clear.addEventListener("click",() =>{
     enteredText = '';
@@ -115,19 +129,142 @@ clear.addEventListener("click",() =>{
     savedNumber.innerText = previousNumber;
 });
 
+//DEL function
 const del = document.getElementById("delete");
 del.addEventListener("click", ()=>{
     enteredText = enteredText.slice(0,-1);
     currentEntry.innerText = enteredText;
 });
 
-// on press: 1.) push information in currently entered into previous, and clear currently entered. 
-//2.) be ready to take in new currentlyEntered
-//3.) apply appropriate function to both variables 
+//ROW TWO
+const sqrt = document.getElementById("sqrt");
+sqrt.addEventListener("click", () =>{
+    previousNumber = `sqr(${enteredText})`;
+    enteredText = Math.sqrt(enteredText);
+    currentEntry.innerText = enteredText;
+    savedNumber.innerText = previousNumber;
+});
+
+//sqrt function
+
+//squared function
+const squared = document.getElementById("squared");
+squared.addEventListener("click", () =>{
+    previousNumber = `sqr(${enteredText})`;
+    enteredText = enteredText **2;
+    currentEntry.innerText = enteredText;
+    savedNumber.innerText = previousNumber;
+});
+
+
+//1/x function
+const oneOverX = document.getElementById("one-over-x");
+oneOverX.addEventListener("click", () => {
+    enteredText = 1 / enteredText;
+    currentEntry.innerText = enteredText;
+    savedNumber.innerText = previousNumber;
+});
 
 
 
-// numButton append to output
-// function buttons do the functions
-// Saved functionality
+//Divide function --- some functionality bugs to be worked out 
+const divide = document.getElementById("divide");
+divide.addEventListener("click",() =>{
+    if(enteredText != "" && previousNumber === "" ){
+        previousNumber = enteredText;
+        enteredText = '';
+        operator = '/';
+        currentEntry.innerText = enteredText;
+        savedNumber.innerText = previousNumber;
+    }
+});
+
+//ROW THREE
+// * function
+const multiply = document.getElementById("multiply");
+multiply.addEventListener("click", ()=>{
+    previousNumber = enteredText;
+    enteredText = '';
+    operator = '*';
+    currentEntry.innerText = enteredText;
+    savedNumber.innerText = previousNumber;
+});
+
+
+//ROW FOUR
+// - function
+const subtract = document.getElementById("subtract");
+subtract.addEventListener("click", ()=>{
+    previousNumber = enteredText;
+    enteredText = '';
+    operator = '-';
+    currentEntry.innerText = enteredText;
+    savedNumber.innerText = previousNumber;
+});
+
+
+//ROW FIVE
+// + function
+const add = document.getElementById("add");
+add.addEventListener("click", ()=>{
+    if(enteredText != ""){
+        previousNumber = enteredText;
+        enteredText = '';
+        operator = '+';
+        currentEntry.innerText = enteredText;
+        savedNumber.innerText = previousNumber;
+    }
+});
+
+//ROW SIX
+
+// positive/negative function
+const posNeg = document.getElementById("pos-neg");
+let isNeg = false;
+posNeg.addEventListener("click", ()=>{
+    isNeg =!isNeg;
+    if(isNeg == true){
+        enteredText = -enteredText;
+    };    
+    currentEntry.innerText = enteredText;
+
+});
+
+//equals function
+// missing functionality for 2nd row function buttons
+const equals = document.getElementById("equals");
+equals.addEventListener("click", ()=>{
+    if(operator == '/'){
+        enteredText = previousNumber/enteredText; // auto converted to number by JS
+        previousNumber = ''
+        currentEntry.innerText = enteredText;
+        savedNumber.innerText = previousNumber;
+    }else if(operator =='+'){
+        enteredText = Number(previousNumber) + Number(enteredText); // had to use Number() becasue + is also for str concat
+            previousNumber = ''
+            currentEntry.innerText = enteredText;
+            savedNumber.innerText = previousNumber;     
+    }else if(operator =='-'){
+        enteredText = Number(previousNumber) - Number(enteredText); // had to use Number() becasue + is also for str concat
+            previousNumber = ''
+            currentEntry.innerText = enteredText;
+            savedNumber.innerText = previousNumber;     
+    }else if(operator =='*'){
+        enteredText = Number(previousNumber) * Number(enteredText); // had to use Number() becasue + is also for str concat
+            previousNumber = ''
+            currentEntry.innerText = enteredText;
+            savedNumber.innerText = previousNumber;     
+    }
+});
+
+
+// }else if(operator == '+'){
+//     enteredText = savedNumber + enteredText;
+// }else if(operator == '-'){
+//     enteredText = savedNumber - enteredText;
+// }else if(operator == '*'){
+//     enteredText = savedNumber * enteredText;
+// }
+    // savedNumber operator currentEntry 
+
 
